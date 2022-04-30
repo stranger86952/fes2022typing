5df9ea996f8359b50b3942ddff76aed5ef91f1b1
stranger86952 <90621530+stranger86952@users.noreply.github.com>
2022-04-30 17:56:42 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,72 @@
-const fs = require('fs');
+//const fs = require('fs');
+var words=[];
+var preview=[];
 
 window.addEventListener('DOMContentLoaded', function(){
   disp('title');
   var rankingdate=[];
   fetch("./list.txt").then(e => e.text()).then(e => rankingA(rankingdate,e));
+  fetch("./words.txt").then(e => e.text()).then(e => words=e.split(/\s/));
+  fetch("./preview.txt").then(e => e.text()).then(e => preview=e.split(/\s/));
 });
+
+function game(){
+  disp('gaming');
+  timer = 60;
+  while(timer>0){
+    var countup = setInterval(function(){
+      timer--;
+      $('.typtim').text('残り時間: ' + String(timer) + '秒');
+    } ,1000);
+  }
+  while($('.typeans').text()==$('.typnow')&&timer>0){
+    ran = Math.floor(Math.random() * ((words.length)–1)+1);
+    $('typans')=words[ran];
+    $('typpre')=preview[ran];
+    var i = 0;
+    $('body').keydown(function(event) {
+      var a = event.key;
+      if(a==words[ran][i]) $('.typnow').append(a);
+    });
+  }
+  if(timer==0){
+    $('.typtim').text('終了！');
+  }
+}
 
 function disp(classname){
   if(classname=='title'){
     $('.title').show();
     $('.game').hide();
+    $('gaming').hide();
     $('.after').hide();
     $('.ranking').hide();
   }
   else if(classname=='game'){
     $('.title').hide();
     $('.game').show();
+    $('gaming').hide();
     $('.after').hide();
+    $('.ranking').hide();
+  }
+  else if(classname=='gaming'){
+    $('.title').hide();
+    $('.game').hide();
+    $('gaming').show();
+    $('.after').hide();
+    $('.ranking').hide();
+  }
+  else if(classname=='after'){
+    $('.title').hide();
+    $('.game').hide();
+    $('gaming').hide();
+    $('.after').show();
     $('.ranking').hide();
   }
   else if(classname=='ranking'){
     $('.title').hide();
     $('.game').hide();
+    $('gaming').hide();
     $('.after').hide();
     $('.ranking').show();
   }
@@ -35,7 +80,7 @@ function rankingA(rankingdate,e){
   }
 }
 
-function rankingB(name,score,typm){
+/*function rankingB(name,score,typm){
   var s="";
   for(var i=0;i<(rankingdate.length-1);i++){
     ran = rankingdate[i].split(/\s/);
@@ -55,4 +100,4 @@ function rankingB(name,score,typm){
     }
   }
   rankingdate.push(score + ' ' + name + ' ' + typm);
-}
+}*/
