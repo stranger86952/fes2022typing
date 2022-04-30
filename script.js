807f7f0dd38225e137b7012a9b7889d0b73cb358
stranger86952 <90621530+stranger86952@users.noreply.github.com>
2022-04-30 11:50:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 window.addEventListener('DOMContentLoaded', function(){
-  fetch("./list.txt").then(e => text()).then(e => $('.ranking').text(e));
+  var rankingdate;
+  fetch("./list.txt").then(e => e.text()).then(e => rankingdate=e.split(/[/\s//\r\n|\n/]/));
   $('#Branking').on('click', function() {
     alert("クリックされました");
   });
+  for(var i=0;i<rankingdate.length;i++){
+    if(i%4==0) $('#ranking').append('<tr><td>' + str(rankingdate[i]) + '位</td>');
+    if(i%4==1) $('#ranking').append('<td>' + str(rankingdate[i]) + '</td>');
+    if(i%4==2) $('#ranking').append('<td>' + str(rankingdate[i]) + '</td></tr>');
+  }
 });
