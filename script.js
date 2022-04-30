9801cc97f849abac322b169d56880eb254e2bc03
stranger86952 <stranger.86952@gmail.com>
2022-04-30 12:39:44 +0900

    auto

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 window.addEventListener('DOMContentLoaded', function(){
+  disp('title');
   var rankingdate=[];
   fetch("./list.txt").then(e => e.text()).then(e => rankingA(rankingdate,e));
   $('#Branking').on('click', function() {
     alert("クリックされました");
   });
 });
+
+function disp(classname){
+  if(classname=='title'){
+    $('.title').show();
+    $('.game').hide();
+    $('.ranking').hide();
+  }
+  else if(classname=='game'){
+    $('.title').hide();
+    $('.game').show();
+    $('.ranking').hide();
+  }
+  else if(classname=='ranking'){
+    $('.title').hide();
+    $('.game').hide();
+    $('.ranking').show();
+  }
+}
 
 function rankingA(rankingdate,e){
   rankingdate=e.split(/[/\s//\r\n|\n/]/)
