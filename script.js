44899ddd4b8f06b831267b0669a0f5275e722fa0
stranger86952 <90621530+stranger86952@users.noreply.github.com>
2022-04-30 11:25:23 +0900

    天才

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', function(){
+  fetch("./list.txt").then(e => text()).then(e => $('.ranking').text(e));
   $('#Branking').on('click', function() {
     alert("クリックされました");
   });
