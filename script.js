7bc9be1f1ce81d969a8cd54053800c2e637bc785
stranger86952 <stranger.86952@gmail.com>
2022-04-30 13:09:17 +0900

    auto

--- script.js
+++ script.js
@@ -1,33 +1,58 @@
+const fs = require('fs');
+
 window.addEventListener('DOMContentLoaded', function(){
   disp('title');
   var rankingdate=[];
   fetch("./list.txt").then(e => e.text()).then(e => rankingA(rankingdate,e));
-  $('#Branking').on('click', function() {
-    alert("クリックされました");
-  });
 });
 
 function disp(classname){
   if(classname=='title'){
     $('.title').show();
     $('.game').hide();
+    $('.after').hide();
     $('.ranking').hide();
   }
   else if(classname=='game'){
     $('.title').hide();
     $('.game').show();
+    $('.after').hide();
     $('.ranking').hide();
   }
   else if(classname=='ranking'){
     $('.title').hide();
     $('.game').hide();
+    $('.after').hide();
     $('.ranking').show();
   }
 }
 
 function rankingA(rankingdate,e){
-  rankingdate=e.split(/[/\s//\r\n|\n/]/)
-  for(var i=0;i<(rankingdate.length-1)/4;i++){
-    $('#ranking').append('<tr><td>' + String(i+1) + '位</td><td>' + String(rankingdate[4*i+1]) + '</td><td>' + String(rankingdate[4*i]) + '</td><td>' + String(rankingdate[4*i+2]) + '</td></tr>');
+  rankingdate=e.split(\r\n);
+  for(var i=0;i<(rankingdate.length-1);i++){
+    ran=rankingdate[i].split(\s);
+    $('#ranking').append('<tr><td>' + String(i+1) + '位</td><td>' + String(ran[0]) + '</td><td>' + String(ran[1]) + '</td><td>' + String(ran[2]) + '</td></tr>');
   }
+}
+
+function rankingB(name,score,typm){
+  var s="";
+  for(var i=0;i<(rankingdate.length-1);i++){
+    ran = rankingdate[i].split(\s);
+    if(ran[0]<=Number(score)){
+      s + String(score) + ' ' + String(name) + ' ' + String(typm) + \r\n;
+      for(var j=i;j<(rankingdate.length-1);j++){
+        s = s + ranking[j] + \r\n;
+      }
+      try {
+        fs.appendFileSync('./list.txt', s, 'utf-8');
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    else{
+      s = s + ranking[i] + \r\n;
+    }
+  }
+  rankingdate.push(score + ' ' + name + ' ' + typm);
 }
